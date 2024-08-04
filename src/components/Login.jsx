b58ca2878d390ad754e9ac5eb21a6067af7ac305
Deepaksen2008 <deepakkumarsen2008@gmail.com>
2024-08-04 17:05:43 +0530

    Add Login Signup page

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import {Link, useNavigate} from 'react-router'
+import {login as authLogin} from '../store/authSlice'
+
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,14 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import Container from './Container/Container';
+import logo from './Logo'
+import LogoutBtn from './Header/LogoutBtn';
 
 export {
     Header,
     Footer,
+    Container,
+    logo,
+    LogoutBtn
     // Add other components here if needed
 }
